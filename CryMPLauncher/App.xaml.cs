955603fb509e8f0291b1d5d68e92127b8e18b1cf
Miroslav <54742822+if-not-skill@users.noreply.github.com>
2024-03-20 00:12:48 +0300

    init commit

---
+++ CryMPLauncher/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CryMPLauncher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ CryMPLauncher/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CryMPLauncher
+{
+    enum LauncherStatus
+    {
+        Ready,
+        Failed,
+        DownloadingGame,
+        DownloadingUpdate
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string _rootPath;
+        private string _versionFile;
+        private string _gameZip;
+        private string _gameExe;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // this.Icon = BitmapFrame.Create(new Uri("pack://application:,,,/Resources/Image/close.png"));
+            _rootPath = Directory.GetCurrentDirectory();
+            _versionFile = Path.Combine(_rootPath, "version.txt");
+            _gameZip = Path.Combine(_rootPath, "CMPPackage.txt");
+            _gameExe = Path.Combine(_rootPath, "Game", "Game.exe");
+        }
+
+        private void ButtonMinimize_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
